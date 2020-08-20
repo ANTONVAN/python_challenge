e60c3a3484e9c86a7061cb2451366d5a9adaaf72
Antonio1234 <aantonio_94@hotmail.com>
2020-08-19 22:24:23 -0500

    mods

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,13 +3,40 @@ import csv
 
 profit_losses = os.path.join("Resources", "Homework 3_PyBank_Resources_budget_data.csv")
 
-def 
 
 with open(profit_losses,"r") as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    csvheader = next(csvfile)
+
+    months_count = 0
+    profit_losses_count = 0 
+    profitLossesList = []
+    prev_row = 0
+    differenceList = []
+
     for row in csvreader:
+        months_count += 1
+        profit_losses_count += int(row[1])
+        profitLossesList.append(int(row[1]))
+
+    for row in profitLossesList:
+        difference = row - prev_row
+        prev_row = row
+        differenceList.append(difference)
+
+    
+    totalMonths = months_count
+    total_profit_losses = profit_losses_count
+    print(f"Total Months: {totalMonths}")
+    print(f"Total: ${total_profit_losses}")
+    #print(profitLossesList)
+    #print(differenceList)
+    print(max(differenceList))
+    print(min(differenceList))
+
+
 
     
 
