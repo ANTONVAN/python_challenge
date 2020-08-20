68ed86394a899215ae2fb2cdd9510d45f45a7cc7
Antonio1234 <aantonio_94@hotmail.com>
2020-08-19 21:02:06 -0500

    N hw

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+import os
+import csv
+
+profit_losses = os.path.join("Resources", "Homework 3_PyBank_Resources_budget_data.csv")
+
+def 
+
+with open(profit_losses,"r") as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    for row in csvreader:
+
+    
+
